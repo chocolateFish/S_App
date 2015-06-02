29047d16fdd19961d71ccb0c5e3af57dbd42cff4
chocolateFish <kiwifarah@gmail.com>
2015-06-02 12:44:12 +1200

    added Main Activity,set as launcher

--- app/src/main/java/sokoban/filer/view/FilerActivity.java
+++ app/src/main/java/sokoban/filer/view/FilerActivity.java
@@ -2,9 +2,12 @@ package sokoban.filer.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.RelativeLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.user.mysokonabapplication.R;
 
@@ -14,13 +17,23 @@ public class FilerActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        myView = new FilerView(this);
-        setContentView(myView);
+        setContentView(R.layout.activity_filer);
+
+        Button saveLevelBtn = (Button)findViewById(R.id.saveLevelBtn);
+
+        saveLevelBtn.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        TextView myTextView = (TextView)findViewById(R.id.saveLevelInput);
+                        myTextView.setText("Button clicked");
+                    }
+                }
+        );
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+         //Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_filer, menu);
         return true;
     }
